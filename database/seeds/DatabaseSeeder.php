3aa447959f19a515d2bfacae86be051d6a19f666
Elu-Thingol <eluthingol@vip.qq.com>
2020-04-10 22:34:20 +0800

    数据库迁移

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(ELU_2020_04_10_214648_DataRowsTableSeeder::class);
         $this->call(ELU_2020_04_10_214648_DataTypesTableSeeder::class);
+        $this->call(ELU_2020_04_10_214648_DataRowsTableSeeder::class);
         $this->call(ELU_2020_04_10_214648_MenuItemsTableSeeder::class);
         $this->call(ELU_2020_04_10_214648_MenusTableSeeder::class);
         $this->call(ELU_2020_04_10_214648_PermissionRoleTableSeeder::class);
