10c89bc131cb4cb0d316d2ffe4b7818f6ebd02ab
Elu-Thingol <eluthingol@vip.qq.com>
2020-04-19 23:11:41 +0800

    优化后台文章内容显示

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\HyperDown\Parser;
+
 if (!function_exists('uint32val')) {
     function uint32val($var)
     {
@@ -34,5 +36,77 @@ if (!function_exists('uright')) {
             $res = $c & ($a >> $n);
         }
         return $res;
+    }
+}
+
+if (!function_exists('parser_makeHtml')) {
+    /**
+     * Return all field translations.
+     *
+     * @param Illuminate\Database\Eloquent\Model $model
+     * @param string                             $field
+     * @param string                             $rowType
+     * @param bool                               $stripHtmlTags
+     */
+    function parser_makeHtml($markdown)
+    {
+        $parser = new Parser;
+
+        return $parser->makeHtml($markdown);
+    }
+}
+
+if (!function_exists('get_field_translations_browse')) {
+    /**
+     * Return all field translations.
+     *
+     * @param Illuminate\Database\Eloquent\Model $model
+     * @param string                             $field
+     * @param string                             $rowType
+     * @param bool                               $stripHtmlTags
+     */
+    function get_field_translations_browse($model, $field, $rowType = '', $stripHtmlTags = false)
+    {
+        $_out = $model->getTranslationsOf($field);
+
+        if ($stripHtmlTags && in_array($rowType, ['text', 'text_area', 'rich_text_box', 'markdown_editor'])) {
+            foreach ($_out as $language => $value) {
+                if ($rowType == 'markdown_editor') {
+                    $_out[$language] = strip_tags(parser_makeHtml($_out[$language]));
+                } elseif ($rowType == 'rich_text_box') {
+                    $_out[$language] = strip_tags($_out[$language]);
+                }
+
+                $_out[$language] = mb_strlen($_out[$language]) > 200 ? mb_substr($_out[$language], 0, 200) . ' ...' : $_out[$language];
+            }
+        }
+
+        return json_encode($_out);
+    }
+}
+
+if (!function_exists('get_field_translations_read')) {
+    /**
+     * Return all field translations.
+     *
+     * @param Illuminate\Database\Eloquent\Model $model
+     * @param string                             $field
+     * @param string                             $rowType
+     * @param bool                               $stripHtmlTags
+     */
+    function get_field_translations_read($model, $field, $rowType = '', $stripHtmlTags = false)
+    {
+        $_out = $model->getTranslationsOf($field);
+        if ($stripHtmlTags && in_array($rowType, ['rich_text_box', 'markdown_editor'])) {
+            foreach ($_out as $language => $value) {
+                if ($rowType == 'markdown_editor') {
+                    $_out[$language] = parser_makeHtml($_out[$language]);
+                }
+
+                $_out[$language] = strip_tags($_out[$language]);
+            }
+        }
+
+        return json_encode($_out);
     }
 }
